6c2b8965ddeda715a498f410875365b91117d39a
soulsam480 <soulsam480@hotmail.com>
2023-12-04 14:21:34 +0530

    feat: init

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+import { TIncomeType, TSavingDebitType } from '@/types';
+
+export const INCOME_TYPES: ReadonlyArray<TIncomeType> = [
+  'salary',
+  'other',
+] as const;
+
+export const SAVING_DEBIT_TYPES: ReadonlyArray<TSavingDebitType> = [
+  'monthly',
+  'yearly',
+  'one-time',
+];
